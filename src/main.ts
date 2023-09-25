b40a2cc95e3fa1db22bb62ebf7aa11b75f074d27
Pedro Conde <pconde007@gmail.com>
2023-09-25 09:25:21 +0100

    Port log addend

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,11 @@ async function bootstrap() {
     })
   );
 
+  const PORT = process.env.PORT ?? 3000;
+
+  console.log(`App corriendo en puerto: ${ PORT }`);
 
 
-
-  await app.listen( process.env.PORT ?? 3000);
+  await app.listen( PORT ?? 3000);
 }
 bootstrap();
